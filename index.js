48ac2fe4bde519f28c9bfa57e5a4041d2f9f8ef8
Ren Lowsphere <78449196+renlowsphere@users.noreply.github.com>
2021-07-09 15:35:12 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+ const io =  new Server(server);
+io.on('connection',(socket)=>{
+console.log("User Connected!");
+ socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+     });
+});
+io.on('disconnect',(socket)=>{
+console.log('User Disconnected');
+});
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html' );
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
